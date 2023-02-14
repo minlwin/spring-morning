2238f7cc0cafb2821756cefe6267f79300ede5ca
Min Lwin <lwin.zawmin@gmail.com>
2023-02-14 08:58:20 +0630

    Update XML Config Project

--- 13-spring5-web/src/main/java/com/jdc/demo/controller/HelloApi.java
+++ 13-spring5-web/src/main/java/com/jdc/demo/controller/HelloApi.java
@@ -1,7 +1,12 @@
 package com.jdc.demo.controller;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jdc.demo.dto.Message;
@@ -9,9 +14,26 @@ import com.jdc.demo.dto.Message;
 @RestController
 @RequestMapping("hello")
 public class HelloApi {
-
+	
 	@GetMapping
 	Message hello() {
 		return new Message("first rest", "Hello Spring MVC");
 	}
+	
+	// GET /hello/generate
+	// @RequestMapping(path = "generate", method = RequestMethod.GET)
+	@GetMapping("generate/{size}/{message}")
+	List<Message> generate(@PathVariable("size") int count, @PathVariable String message) {
+		return IntStream.iterate(0, a -> a + 1).limit(count)
+				.mapToObj(num -> new Message("Key %d".formatted(num), "%s %d".formatted(message, num)))
+				.toList();
+	}
+	
+	@GetMapping("generate")
+	List<Message> generateWithRequestParam(@RequestParam int count, @RequestParam String message) {
+		return IntStream.iterate(0, a -> a + 1).limit(count)
+				.mapToObj(num -> new Message("Key %d".formatted(num), "%s %d".formatted(message, num)))
+				.toList();
+	}
+	
 }
